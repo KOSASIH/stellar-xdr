f3c09c078bb1cfa6278bcbd424c63d21c03f86de
KOSASIH <kosasihg88@gmail.com>
2025-01-04 14:15:47 +0700

    Create xdr_validator.py

---
+++ src/xdr_validator.py
@@ -0,0 +1,12 @@
+# src/xdr_validator.py
+
+class XDRValidator:
+    @staticmethod
+    def validate(data):
+        # Implement validation logic
+        pass
+
+    @staticmethod
+    def validate_field(field, value):
+        # Validate individual fields
+        pass
