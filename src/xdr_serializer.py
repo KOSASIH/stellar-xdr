17ef35a6ee250baf79398429cb455121bbbe8415
KOSASIH <kosasihg88@gmail.com>
2025-01-04 14:14:46 +0700

    Create xdr_serializer.py

---
+++ src/xdr_serializer.py
@@ -0,0 +1,24 @@
+# src/xdr_serializer.py
+
+import struct
+
+class XDRSerializer:
+    @staticmethod
+    def serialize(data):
+        # Implement serialization logic
+        pass
+
+    @staticmethod
+    def deserialize(data):
+        # Implement deserialization logic
+        pass
+
+    @staticmethod
+    def batch_serialize(data_list):
+        # Serialize a list of data
+        pass
+
+    @staticmethod
+    def batch_deserialize(data_list):
+        # Deserialize a list of data
+        pass
